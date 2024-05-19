d1148f6f709b196e5c6c772f8a3aefb0328da5c0
arthurpansera <arthurpansera0308@gmail.com>
2024-05-19 19:43:30 -0300

    Calculadora
    PBL 4 - Calculadora RMC

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,51 @@
 #Projeto: Calculadora RMC
 #Alunos: Arthur Rodrigues Pansera e Stefany Carlos de Oliveira
 #Turma: C
+
+def adicao(a, b):
+    return a + b
+
+def subtracao(a, b):
+    return a - b
+
+def multiplicacao(a, b):
+    return a * b
+
+def divisao(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Não é possível dividir por zero!"
+
+def calculadora():
+    while True:
+        print(50*"-")
+        print("Selecione a operação:")
+        print("1. Adição")
+        print("2. Subtração")
+        print("3. Multiplicação")
+        print("4. Divisão")
+        print("5. Sair")
+        escolha = input("Digite sua escolha: ")
+        if escolha in ('1', '2', '3', '4'):
+            num1 = float(input("Digite o primeiro número: "))
+            num2 = float(input("Digite o segundo número: "))
+            if escolha == '1':
+                print(f"Resultado: {adicao(num1, num2)}")
+            elif escolha == '2':
+                print(f"Resultado: {subtracao(num1, num2)}")
+            elif escolha == '3':
+                print(f"Resultado: {multiplicacao(num1, num2)}")
+            elif escolha == '4':
+                while num2 == 0:
+                    print("Opção inválida! Impossível dividir por 0!")
+                    num1 = float(input("Digite o primeiro número: "))
+                    num2 = float(input("Digite o segundo número: "))
+                print(f"Resultado: {divisao(num1, num2)}")
+        elif escolha == '5':
+            print("Saindo...")
+            break
+        else:
+            print("Escolha inválida! Escolha uma das opções.")
+
+calculadora()
