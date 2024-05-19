228fa678aacf292ef9bab4c5eef19689f1c80d7e
arthurpansera <arthurpansera0308@gmail.com>
2024-05-19 20:42:21 -0300

    Calculadora
    PBL 4 - Calculadora RMC

--- calculadora.py
+++ calculadora.py
@@ -17,35 +17,55 @@ def divisao(a, b):
     else:
         return "Não é possível dividir por zero!"
 
-def calculadora():
-    while True:
-        print(50*"-")
-        print("Selecione a operação:")
-        print("1. Adição")
-        print("2. Subtração")
-        print("3. Multiplicação")
-        print("4. Divisão")
-        print("5. Sair")
-        escolha = input("Digite sua escolha: ")
-        if escolha in ('1', '2', '3', '4'):
-            num1 = float(input("Digite o primeiro número: "))
-            num2 = float(input("Digite o segundo número: "))
-            if escolha == '1':
-                print(f"Resultado: {adicao(num1, num2)}")
-            elif escolha == '2':
-                print(f"Resultado: {subtracao(num1, num2)}")
-            elif escolha == '3':
-                print(f"Resultado: {multiplicacao(num1, num2)}")
-            elif escolha == '4':
-                while num2 == 0:
-                    print("Opção inválida! Impossível dividir por 0!")
-                    num1 = float(input("Digite o primeiro número: "))
-                    num2 = float(input("Digite o segundo número: "))
-                print(f"Resultado: {divisao(num1, num2)}")
-        elif escolha == '5':
-            print("Saindo...")
-            break
-        else:
-            print("Escolha inválida! Escolha uma das opções.")
+def operacoes_basicas():
+    print(50*"-")
+    print("Selecione a operação:")
+    print("1. Adição")
+    print("2. Subtração")
+    print("3. Multiplicação")
+    print("4. Divisão")
+    print("5. Voltar")
+    escolha = input("Digite sua escolha: ")
+    if escolha in ('1', '2', '3', '4'):
+        num1 = float(input("Digite o primeiro número: "))
+        num2 = float(input("Digite o segundo número: "))
+        if escolha == '1':
+            print(f"Resultado: {adicao(num1, num2)}")
+        elif escolha == '2':
+            print(f"Resultado: {subtracao(num1, num2)}")
+        elif escolha == '3':
+            print(f"Resultado: {multiplicacao(num1, num2)}")
+        elif escolha == '4':
+            while num2 == 0:
+                print("Opção inválida! Impossível dividir por 0!")
+                num1 = float(input("Digite o primeiro número: "))
+                num2 = float(input("Digite o segundo número: "))
+            print(f"Resultado: {divisao(num1, num2)}")
+    elif escolha == '5':
+        print("Voltando...")
+    else:
+        print("Escolha inválida! Escolha uma das opções.")
 
-calculadora()
+def sair():
+    print("Saindo...")
+    exit()
+
+def menu_opcoes():
+    print(50*"-")
+    print("Escolha uma das opções:")
+    print("1. Operações básicas")
+    print("2. Conjuntos numéricos")
+    print("3. Funções do segundo grau")
+    print("4. Funções exponenciais")
+    print("5. Matrizes")
+    print("6. Sair")
+    opcao = input("Digite sua escolha: ")
+    if opcao == '1':
+        operacoes_basicas()
+    elif opcao == '6':
+        sair()
+    else:
+        print("Escolha inválida! Escolha uma das opções.")
+
+while True:
+    menu_opcoes()
